2f609ee42511a0fbcdd30d0eea051e15684dcdaf
Marek Żochowski <marrkzochowski2@gmail.com>
2021-12-21 23:17:53 +0100

    Refactor e2e tests

--- e2e_tests.py
+++ e2e_tests.py
@@ -12,19 +12,20 @@ linter_client = TestClient(linter_app)
 
 
 class E2eTests(unittest.TestCase):
-
     @staticmethod
     def start_machine_manager():
         _, machine_manager_url = deploy_utils.start_fast_api_app("machine_manager")
         return machine_manager_url
 
+    def setUp(self) -> None:
+        self.machine_manager_url = E2eTests.start_machine_manager()
+
     def test_spawning_linters(self):
-        machine_manager_url = E2eTests.start_machine_manager()
-        deploy_utils.start_fast_api_app("machine_manager")
         for i in range(10):
-            machine_manager_api.deploy_linter_instance(machine_manager_url, "1.0", None)
-        result = machine_manager_api.get_machines(machine_manager_url)
+            machine_manager_api.deploy_linter_instance(self.machine_manager_url, "1.0", None)
+        result = machine_manager_api.get_machines(self.machine_manager_url)
         self.assertEqual(10, len(result))
+
         for linter_instance in result:
             response: LinterResponse = LinterResponse.from_dict(
                 linter_api.validate(linter_instance.address, LinterRequest(language="python", code="x=5")))
