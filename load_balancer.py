2ef8555aa54f9a1332979fbc6d1c909a3d9379d7
Tomasz Grześkiewicz <tomasz.grzeskiewicz@vavatech.pl>
2022-01-13 15:40:53 +0100

    merge fix

--- load_balancer.py
+++ load_balancer.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 
 from fastapi import FastAPI
 from multiprocessing import Lock
@@ -35,28 +36,23 @@ def validate_file(request: LinterRequest) -> LinterResponse:
     linters = machine_manager_api.get_linters(machine_manager_url)
     linters.sort(key=lambda linter: linter.instance_id)
 
-    machines = machine_manager_api.get_machines(machine_manager_url)
-
-    if len(machines) == 0:
-        return LinterResponse(result="fail", errors=["No linter machine available"], debug=[])
+    if len(linters) == 0:
+        return LinterResponse(result="fail", errors=["No linter instance available"], debug=[])
 
     retries_count = int(os.environ.get("LOAD_BALANCER_RETRIES_COUNT", 3))
     for _ in range(retries_count):
-        local_linter_number = 0
-        try:
-            lock.acquire()
+        lock.acquire()
+        local_linter_number = linter_number
 
-            if linter_number >= len(linters):
-                linter_number = 0
-
-            local_linter_number = linter_number
-            linter_number += 1
-        finally:
-            lock.release()
+        linter_number += 1
+        if linter_number >= len(linters):
+            linter_number = 0
+        lock.release()
 
         try:
-            return linter_api.validate(machines[local_linter_number - 1].address, request)
+            return linter_api.validate(linters[local_linter_number - 1].address, request)
         except:
+            print(traceback.format_exc())
             continue
 
-    return LinterResponse(result="fail", errors=["No linter machine available"], debug=[])
+    return LinterResponse(result="fail", errors=["No linter instance was able to handle request"], debug=[])
