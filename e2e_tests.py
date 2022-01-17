5e20fb7306d8110ecd1f2698510c18a54a505307
mkmkl93 <mkmkl93@gmail.com>
2022-01-17 10:45:37 +0100

    Fix opening on taken port (#26)
    * Fix opening application on already used port
    
    * Add test for opening 2 applications on the same socket

--- e2e_tests.py
+++ e2e_tests.py
@@ -263,6 +263,13 @@ class E2eTests(unittest.TestCase):
             response = load_balancer_api.validate(self.load_balancer_url, request=E2eTests.flawless_python_request)
             self.assertEqual("ok", response.result)
 
+    def test_opening_application_on_already_occupied_socket(self):
+        machine_manager_process, _ = deploy_utils.start_fast_api_app("machine_manager", port=5000)
+        load_balancer_process, _ = deploy_utils.start_fast_api_app("load_balancer", port=5000)
+
+        deploy_utils.stop_fast_api_app(machine_manager_process)
+        deploy_utils.stop_fast_api_app(load_balancer_process)
+
 
 if __name__ == "__main__":
     unittest.main()
